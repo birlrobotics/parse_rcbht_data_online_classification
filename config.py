c0f3d50812d8ee12a0b331103a79aede57daa1c2
sklaw <sk.law.lsq@gmail.com>
2016-12-09 09:04:37 +0800

    welcome failure class trainer

--- config.py
+++ config.py
@@ -13,3 +13,16 @@ proba_threshold = 0.95
 
 model_dir = "./models_by_method"
 model_for_states = "svm/model_for_state_classification_dataType_REAL_method_svm_accuracy_92_avgProbaOfAll_73_avgByClass_C0P88_C1P72_C2P67_C3P64_.pkl"
+
+failure_x_base = ["+x", ""]
+failure_y_base = ["+y", "-y", ""]
+failure_r_base = ["+r", "-r", ""]
+
+failure_class_name_to_id = {}
+id_count = 0
+for x in failure_x_base:
+    for y in failure_y_base:
+        for r in failure_r_base:
+            now_class = x+y+r
+            failure_class_name_to_id[now_class] = id_count
+            id_count += 1
